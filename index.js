a095341a0213c8da96248d2c97b3b3112ba45fd4
Abraham Bekele <abb2007hu@gmail.com>
2021-09-20 23:01:02 +0300

    fix

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 
 // app.listen(port, () => {
 app.listen(() => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost`)
 })
